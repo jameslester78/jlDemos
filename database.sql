d6b4caefab0ad90681d2944cb7a76aa8af3a57b4
unknown <jameslester78@gmail.com>
2023-08-31 17:09:21 +0100

    added the special-events html to sampleData

--- database.sql
+++ database.sql
@@ -1,3 +1,19 @@
 CREATE VIEW parkrun_project1.firstRunAtEvent AS
 SELECT event,min(rundate) AS rundate FROM completedRuns GROUP BY event
 ;
+CREATE VIEW parkrun_project1.allEvents
+AS
+SELECT    a.eventShortName,
+          localAuthority,
+          county,
+          region,
+          sss,
+          travelTimeMins,
+          travelDistanceKM,
+          runDate
+          
+FROM      events a
+LEFT JOIN firstRunAtEvent b ON a.eventShortName = b.event
+LEFT JOIN sss c ON c.event = a.eventShortName
+LEFT JOIN drivingDistances d ON d.eventShortName = a.eventShortName
+LEFT JOIN regions e ON e.eventLongName = a.eventLongName
